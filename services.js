00ffc699cf7ba3d385c7da97cb61c537ea02ccbe
Ryan <ryan.o.mackey@gmail.com>
2016-08-02 10:57:24 -0600

    added filter feature

--- services.js
+++ services.js
@@ -4,17 +4,27 @@ app.service('OmdbSearch', ['$http', function($http) {
   var vm = this;
   vm.searchResults = [];
   vm.movie = {};
-  this.search = function(searchQuery) {
-    $http.get('http://www.omdbapi.com/?s=' + searchQuery)
-    .then(function(data) {
-      vm.searchResults = data.data.Search;
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
+  vm.searchFilter = '';
+  this.search = function(searchQuery, searchFilter) {
+    if (searchFilter) {
+      $http.get('http://www.omdbapi.com/?s=' + searchQuery + '&type=' + searchFilter)
+      .then(function(data) {
+        vm.searchResults = data.data.Search;
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+    } else {
+      $http.get('http://www.omdbapi.com/?s=' + searchQuery)
+      .then(function(data) {
+        vm.searchResults = data.data.Search;
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+    }
   };
   this.movieSearch = function(imdbID) {
-    console.log(imdbID);
     $http.get('http://www.omdbapi.com/?i=' + imdbID)
     .then(function(data) {
       vm.movie = data.data;
